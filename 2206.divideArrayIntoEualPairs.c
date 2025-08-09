a19fe5160a74f13bd7848d21ddacd7b168c21e3f
Aranya5 <aranyabasu5@gmail.com>
2025-08-09 20:32:24 +0530

    Given array is divided into arrys of size 2 with equal elements

---
+++ 2206.divideArrayIntoEualPairs.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool divideArray(int* nums, int numsSize) {
+    int freq[501] = {0}; // given constraints: 1 <= nums[i] <= 500
+    
+    // Count frequency of each number
+    for (int i = 0; i < numsSize; i++) {
+        freq[nums[i]]++;
+    }
+    
+    // Check if every number appears an even number of times
+    for (int i = 0; i < 501; i++) {
+        if (freq[i] % 2 != 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int main() {
+    int nums[] = {3, 2, 3, 2, 2, 2}; // Example
+    int size = sizeof(nums) / sizeof(nums[0]);
+
+    if (divideArray(nums, size)) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
+
+    return 0;
+}
